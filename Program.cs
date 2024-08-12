d26522415aed0e9ce1c19cdb1a3a734d1e681474
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-12 17:37:13 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace ShowPrimeNumb;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 2; i <= 100; i++)
+        {
+            if (i < 4)
+            {
+                Console.Write($"{i} ");
+            }
+            else
+            {
+                bool isPrime = true;
+                for (int j = 2; j <= MathF.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            }
+        }
+    }
